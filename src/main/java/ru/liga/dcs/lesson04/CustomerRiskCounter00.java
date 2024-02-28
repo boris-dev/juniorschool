51badb2d45dfdb280670da55de36acc18e7149f0
boris-dev <boris.shestakov@gmail.com>
2024-02-28 16:17:24 +0300

    lesson03

---
+++ src/main/java/ru/liga/dcs/lesson04/CustomerRiskCounter00.java
@@ -0,0 +1,23 @@
+package ru.liga.dcs.lesson04;
+
+import ru.liga.dcs.lesson04.domain.Customer00;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Класс для подсчета количества клиентов по категориям риска.
+ */
+public class CustomerRiskCounter00 {
+
+    /**
+     * Подсчитывает количество клиентов в каждой категории риска.
+     * 
+     * @param customers Список клиентов.
+     * @return Map, где ключ - это категория риска, а значение - количество клиентов в этой категории.
+     */
+    public static Map<String, Integer> countCustomersByRiskCategory(List<Customer00> customers) {
+        return null;
+    }
+}
